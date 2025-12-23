d3f8ff33ed232e2e96ab61ce4b7d314b09a5d56a
Akshata <161721573+Akshataph03@users.noreply.github.com>
2025-12-23 12:16:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// hello-world.js
+export default () => {
+  return new Response("Hello from Supabase via file_url!", {
+    status: 200,
+    headers: { "Content-Type": "text/plain" },
+  });
+};
 
